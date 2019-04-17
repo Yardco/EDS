1f7f186314dfbc04118aac2062d5557ba3c60224
Jayce Yard <jayce.yard@essent.nl>
2019-04-17 09:19:31 +0200

    UPDATED BUTTON COLOR

--- hook/up-eds-version.sh
+++ hook/up-eds-version.sh
@@ -2,13 +2,13 @@
 
 regex="sparkle:version=\"([0-9]+)\""
 
-if [[ `cat $PWD/eds.xml` =~ $regex ]]
+if [[ `cat "$PWD/eds.xml"` =~ $regex ]]
 then
     original=${BASH_REMATCH[1]}
     new=$original
     ((new++))
 
-    sed -i s/sparkle:version=\"$original\"/sparkle:version=\"$new\"/ $PWD/eds.xml
+    sed -i s/sparkle:version=\"$original\"/sparkle:version=\"$new\"/ "$PWD/eds.xml"
     echo "Upped the version of eds.xml to $new"
 fi
 
